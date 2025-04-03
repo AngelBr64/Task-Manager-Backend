8ba69c6075bdc8466e2f41e1444ad09df31e4251
Angel <2022371037@uteq.edu.mx>
2025-04-02 21:25:27 -0600

    Resueltos conflictos de fusión

--- server.js
+++ server.js
@@ -6,11 +6,27 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+const allowedOrigins = [
+  'https://task-manager-frontend-five-chi.vercel.app',
+  'http://localhost:3000' // Add other environments as needed
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Required when using withCredentials
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Rutas
-app.use('/api/auth', authRoutes);
+app.use('/api/', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
