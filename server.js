2e2fb1a9cebecc288258ab5b9f09520ed92d013e
Angel <2022371037@uteq.edu.mx>
2025-04-02 22:25:06 -0600

    Fix: update package-lock.json

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// backend/server.js
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
@@ -5,15 +6,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = 5000;
 
-// Configuración CORS
-const corsOptions = {
-  origin: 'https://task-manager-frontend-five-chi.vercel.app',  // La URL de tu frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Los métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'],  // Los encabezados permitidos
-  credentials: true,  // Si tu aplicación maneja cookies o credenciales
-};
-
-app.use(cors(corsOptions));  // Aplica la configuración CORS
+app.use(cors());
 app.use(express.json());
 
 // Rutas
